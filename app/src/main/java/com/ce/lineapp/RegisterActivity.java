bb0b2417968289f7295ec692f5b8c105590664a2
caev03 <ca.escobar2434@uniandes.edu.co>
2016-04-25 11:39:35 -0500

    - Selección de fecha de nacimiento por medio de un dialogo.

--- app/src/main/java/com/ce/lineapp/RegisterActivity.java
+++ app/src/main/java/com/ce/lineapp/RegisterActivity.java
@@ -1,11 +1,22 @@
 package com.ce.lineapp;
 
+import android.app.DatePickerDialog;
 import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.view.View;
 
-public class RegisterActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
+
+    Calendar myCalendar = Calendar.getInstance();
+
+    EditText txtFechaNac;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +32,34 @@ public class RegisterActivity extends AppCompatActivity {
         EditText txtContrasena = (EditText) findViewById(R.id.txtContrasena);
         txtContrasena.getBackground().mutate().setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
         txtContrasena.setHintTextColor(getResources().getColor(R.color.white));
-        EditText txtFechaNac = (EditText) findViewById(R.id.txtFechaNac);
+        txtFechaNac = (EditText) findViewById(R.id.txtFechaNac);
         txtFechaNac.getBackground().mutate().setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
         txtFechaNac.setHintTextColor(getResources().getColor(R.color.white));
 
+        txtFechaNac.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        DatePickerDialog dialog = new DatePickerDialog(this, R.style.DatePickerDialog, this, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH));
+        dialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+    {
+        myCalendar.set(Calendar.YEAR, year);
+        myCalendar.set(Calendar.MONTH, monthOfYear);
+        myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        txtFechaNac.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    public void registrarUsuario(View v)
+    {
+        finish();
     }
 }
